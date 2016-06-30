e3d6b3943bd7f194b8f03e4126ced9d152178c57
DrUlysses <DrUlysses@users.noreply.github.com>
2016-06-30 18:26:14 +0400

    Add files via upload
    Добавил комментарии и id

--- kritkod/src/application/Target.java
+++ kritkod/src/application/Target.java
@@ -11,60 +11,73 @@ import javafx.scene.control.TextField;
 //Цель
 public class Target {
 	
+	private int id;
 	public static LinkedHashSet<Task> TaskList = new LinkedHashSet<Task>();
 	private String label = "";
 	private String description = "";
 	LocalDate startDate = LocalDate.now();
 	LocalDate endDate = LocalDate.now();
-	
+	//Назначить айди цели
+	public void setId(int idNumber) {
+		
+		id = idNumber;
+		
+	}
+	//Создать задачу для данной цели
 	public void createTask(Task task) {
 		
 		TaskList.add(task);
 		
 	}
-	
+	//Назначить заголовок цели
 	public void setLabel(TextField addNameTarget) throws IOException {
 		
 		label = addNameTarget.getText();
 		
 	}
-	
+	//Назначить описание цели
 	public void setDescription(TextArea addDiscriptionTarget) throws IOException {
 		
 		description = addDiscriptionTarget.getText();
 		
 	}
-	
+	//Назначить начальную дату цели
 	public void setStartDate(DatePicker addDataTarget) {
 		
 		startDate = addDataTarget.getValue();
 		
 	}
-	
+	//Назначить конечную дату цели
 	public void setEndDate(DatePicker addDataTarget) {
 		
 		endDate = addDataTarget.getValue();
 		
 	}
-	
+	//Получить айди цели
+	public int getId(Target target) {
+		
+		return target.id;
+		
+	}
+	//Получить заголовок цели
 	public String getLabel() {
 		
 		return label;
 		
 	}
-	
+	//Получить описание цели
 	public String getDescription() {
 		
 		return description;
 		
 	}
-	
+	//Получить начальную дату цели
 	public LocalDate getStartDate() {
 		
 		return startDate;
 		
 	}
-	
+	//Получить конечную дату цели
 	public LocalDate getEndDate() {
 		
 		return endDate;
--- kritkod/src/application/Task.java
+++ kritkod/src/application/Task.java
@@ -14,52 +14,65 @@ import javafx.scene.control.TextField;
 //Задача
 public class Task {
 	
+	private int id;
 	private String label = "";
 	private String description = "";
 	LocalDate startDate = LocalDate.now();
 	LocalDate endDate = LocalDate.now();
-	
+	//Назначить айди задачи
+	public void setId(int idNumber) {
+		
+		id = idNumber;
+		
+	}
+	//Назначить заголовок задачи
 	public void setLabel(TextField addNameTask) throws IOException { 
 
 		label = addNameTask.getText(); 
 
 	} 
-
+	//Назначить описание задачи
 	public void setDescription(TextArea addDiscriptionTask) throws IOException { 
 
 		description = addDiscriptionTask.getText(); 
 
 	}
-	
+	//Назначить начальную дату задачи
 	public void setStartDate(DatePicker addDataTask) {
 		
 		startDate = addDataTask.getValue();
 		
 	}
-	
+	//Назначить конечную дату задачи
 	public void setEndDate(DatePicker addDataTask) {
 		
 		endDate = addDataTask.getValue();
 		
 	}
-	
+	//Получить айди задачи
+	public int getId(Task task) {
+		
+		return task.id;
+		
+	}
+	//Получить заголовок задачи
 	public String getLabel() {
 		
 		return label;
 	}
-	
+	//Получить описание задачи
 	public String getDescription() {
 		
 		return description;
 		
 	}
-	
+	//Получить начальную дату задачи
 	public LocalDate getStartDate() {
 		
 		return startDate;
 		
 	}
-	
+	//Получить конечную дату задачи
 	public LocalDate getEndDate() {
 		
 		return endDate;
