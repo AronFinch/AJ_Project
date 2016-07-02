dc703e90074ab845617e1b6812d9ca764306653d
karpo <karpo@192.168.1.7>
2016-07-03 00:57:04 +0300

    Добавил функционал: Записи пользователя в БД(по логину и паролю/по логину), Считывания пользователя с БД, Выведения секретного вопроса, сравнение секретного ответа с БД, изменение пароля.

--- kritkod/src/model/DataBaseManager.java
+++ kritkod/src/model/DataBaseManager.java
@@ -1,37 +1,69 @@
 package model;
 import java.sql.*;
-import java.time.LocalDate;
 /*
  * Класс для работы с базой данных
  */
 public class DataBaseManager {
 	private static Connection conn;
-	private static Statement statmt;
 	private static ResultSet resSet;
 	
-	// подключение к базе данных
-	public static void Connect() throws ClassNotFoundException, SQLException {
-		conn = null;
-		Class.forName("org.sqlite.JDBC");
-		conn = DriverManager.getConnection("jdbc:sqlite:testBD.db3");
-		statmt = conn.createStatement();
+	// Установление соеденения
+	public static void Connect(){
+		try {
+			Class.forName("org.sqlite.JDBC"); // регистрация драйвера
+			conn = DriverManager.getConnection("jdbc:sqlite:testBD.db3");
+			//получение соединения
+		} catch(ClassNotFoundException CNFex) {
+			CNFex.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	//метод получения пользователя из базы данных по логину и паролю В resSet хранятся интересующие нас параметры
-	public static ResultSet BDGetUser(String login, String password) throws SQLException {
-		String query = "SELECT id_user, name_user, date_of_birth, gender FROM users "
+	//методы получения пользователя из базы данных по логину и паролю 
+	public static void BDGetUser(String login, String password, User user) throws SQLException{
+		Statement statmt = conn.createStatement();
+		String query = "SELECT id_user, name_user, date_of_birth, gender, rating "
+				+ "FROM users "
 				+ "where login_user='" + login + "' AND password_user='" + password +"'";
 		resSet = statmt.executeQuery(query);
-		return resSet;
+		if(resSet.next()) {
+			user.setId(resSet.getInt("id_user"));
+			user.setName(resSet.getString("name_user"));
+			user.setBirthDate(resSet.getDate("date_of_birth").toLocalDate());
+			user.setGender(resSet.getString("gender").charAt(0));
+			user.setRating(resSet.getInt("rating"));
+		}
+	}
+	public static void BDGetUser(String login, User user) throws SQLException {
+		Statement statmt = conn.createStatement();
+		String query = "SELECT id_user, name_user, date_of_birth, gender, rating "
+				+ "FROM users "
+				+ "where login_user='" + login + "'";
+		resSet = statmt.executeQuery(query);
+		if(resSet.next()) {
+			user.setId(resSet.getInt("id_user"));
+			user.setName(resSet.getString("name_user"));
+			user.setBirthDate(resSet.getDate("date_of_birth").toLocalDate());
+			user.setGender(resSet.getString("gender").charAt(0));
+			user.setRating(resSet.getInt("rating"));
+		}
 	}
 	//метод записи пользователя в базу данных
-	public static void BDAddUser(String login, String password, String secriteQuestion, String answer, User user) throws SQLException {
+	public static void BDAddUser(String login,
+								 String password,
+								 String secriteQuestion,
+								 String answer,
+								 User user) throws SQLException{
+		
+		Statement statmt = conn.createStatement();
 		String query = "INSERT INTO 'users' "
 				+ "(login_user, "
 				+ "password_user, "
 				+ "name_user, "
 				+ "date_of_birth, "
 				+ "gender, "
+				+ "rating, "
 				+ "secrite_question, "
 				+ "answer) "
 				+ "VALUES "
@@ -40,13 +72,54 @@ public class DataBaseManager {
 				+ "'" + user.getName() + "', "
 				+ "'" + java.sql.Date.valueOf(user.getBirthDate()) + "', "
 				+ "'" + user.getGender() + "', "
+				+ "" + user.getRating() + ", "
 				+ "'" + secriteQuestion + "', "
 				+ "'" + answer + "')";
-		statmt.execute(query);
+		statmt.execute(query); // тут мы сохраняем бд 
+		query = "SELECT id_user FROM users "
+					+ "where login_user='" + login + "'";
+		resSet = statmt.executeQuery(query);
+		if(resSet.next()) {
+			user.setId(resSet.getInt("id_user")); // тут мы получаем id сохранённого пользователя
+		}
 	}
-
-	public static void Disconnect() throws SQLException {
-		// хз зачем я создал этот метод
+	// загрузка из БД вопроса
+	public static String BDGetQuestion(int id_user) throws SQLException {
+		Statement statmt = conn.createStatement();
+		String query = "SELECT secrite_question FROM users "
+				+ "where id_user=" + id_user;
+		resSet = statmt.executeQuery(query);
+		if(resSet.next()) {
+			return resSet.getString("secrite_question");
+		} else {
+			return "";
+		}
+	}
+	//загрузка из БД ответа
+	public static String BDGetAnswer(int id_user) throws SQLException {
+		Statement statmt = conn.createStatement();
+		String query = "SELECT answer FROM users "
+				+ "where id_user=" + id_user;
+		resSet = statmt.executeQuery(query);
+		if(resSet.next()) {
+			return resSet.getString("answer");
+		} else {
+			return "";
+		}
+	}
+	public static void BDUpdatePassword(String newPassword, int id_user) throws SQLException { 
+		Statement statmt = conn.createStatement();
+		String query = "UPDATE 'users' "
+				+ "SET password_user='" + newPassword + "' "
+				+ "WHERE id_user='" + id_user + "'";
+		statmt.executeUpdate(query);
+	}
+	public static void Disconnect() {
+		try {
+			conn.close();
+		} catch(SQLException SQLex) {
+			SQLex.printStackTrace();
+		}
 	}
 }
 
--- kritkod/src/model/User.java
+++ kritkod/src/model/User.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 
@@ -9,85 +10,141 @@ import javafx.scene.control.DatePicker;
 public class User {
 
 	private int id;
-	private String login = "";
-	private String password = "";
-	private String name = "";
-	private char gender = 'f';
+	private String name;
 	private LocalDate birthDate = LocalDate.now();
-	public static LinkedHashSet<Notice> NoticeList = new LinkedHashSet<Notice>();
-	public static LinkedHashSet<Statistics> StatisticsList = new LinkedHashSet<Statistics>();
+	private char gender;
+	private int rating;
 	
+	public Statistics statistics = new Statistics();
+	public LinkedHashSet<Notice> NoticeList = new LinkedHashSet<Notice>();
+	public LinkedHashSet<Target> TargetList = new LinkedHashSet<Target>();
+	/*
+	 * конструктор по умолчанию
+	 * создаЄт пользовател€ 
+	 * после зоздани€ нужно будет прогрузить из базы данных, или самому заполнить пол€
+	 */
+	public User() {
+		Clear();
+	}
+	
+	/*
+	 * метод заполн€ющий класс дефолтными результатами
+	 */
+	public void Clear() {
+		id = 0; // нумераци€ в базе с 1
+		name = "пользователь не найден"; // 
+		birthDate = LocalDate.of(1000, 1, 1); // пусть будет така€ дата
+		gender = 'h'; //Genderfluid Helisexual
+		rating = 0; //дефолтное значение в базе 300
+	}
+	
+	/*
+	 * метод сохранени€ пользовател€ в базе данных
+	 */
+	public void SaveUser(String login,
+						 String password,
+						 String secriteQuestion,
+						 String answer) throws SQLException {
+		DataBaseManager.Connect();
+		DataBaseManager.BDAddUser(login, password, secriteQuestion, answer, this);
+		DataBaseManager.Disconnect();
+	}
+	/*
+	 * методы загрузки пользовател€ из базы данных
+	 */
+	public void loadUser(String login, String password) throws SQLException {
+		DataBaseManager.Connect();
+		DataBaseManager.BDGetUser(login, password, this);
+		DataBaseManager.Disconnect();
+	}
+	public void loadUser(String login) throws SQLException {
+		DataBaseManager.Connect();
+		DataBaseManager.BDGetUser(login, this);
+		DataBaseManager.Disconnect();
+	}
+	/*
+	 * метод изменени€ парол€
+	 */
+	public void changePassword(String newPassword) throws SQLException {
+		DataBaseManager.Connect();
+		DataBaseManager.BDUpdatePassword(newPassword, id);
+		DataBaseManager.Disconnect();	
+	}
 	//—оздать уведомление дл€ пользовател€
 	public void createNotice(Notice notice) {
-		
 		NoticeList.add(notice);
-		
-	}
-	//—оздать пункт статистики дл€ пользовател€
-	public void createStatistics(Statistics statistics) {
-		
-		StatisticsList.add(statistics);
-		
-	}
-	//Ќазначить айди пользовател€
-	public void setId(int idNumber) {
-		
-		id = idNumber;
-		
-	}
-	//Ќазначить логин пользовател€
-	public void setLogin(String log) {
-		
-		login = log;
-		
-	}
-	//Ќазначить пароль пользовател€
-	public void setPassword(String pass) {
-		
-		password = pass;
-		
-	}
-	//Ќазначить им€ пользовател€
-	public void setName(String fullName) {
-		
-		name = fullName;
-		
-	}
-	//Ќазначить пол пользовател€
-	public void setGender(char gen) {
-		
-		gender = gen;
-		
-	}
-	//Ќазначить дату рождени€ пользовател€
-	public void setBirthDate(DatePicker addDataTarget) {
-		
-		birthDate = addDataTarget.getValue();
-		
-	}
-	//ѕолучить дату рождени€ пользовател€
-	public LocalDate getBirthDate() {
-		
-		return birthDate;
-		
-	}
-	//ѕолучить пол пользовател€
-	public char getGender() {
-		
-		return gender;
-		
-	}
-	//ѕолучить им€ пользовател€
-	public String getName() {
-		
-		return name;
-		
-	}
-	//ѕолучить айди пользовател€
-	public int getId(User user) {
-		
-		return user.id;
-		
 	}
 	
+	//—оздать пункт статистики дл€ пользовател€
+	public void setStatistics(Statistics statistics) {
+		this.statistics = statistics;
+	}
+	
+	//Ќазначить айди пользовател€
+	public void setId(int idNumber) {	
+		id = idNumber;
+	}
+	
+	//Ќазначить им€ пользовател€
+	public void setName(String fullName) {
+		name = fullName;
+	}
+	
+	//Ќазначить дату рождени€ пользовател€
+	public void setBirthDate(LocalDate dateOfBirth) {
+		birthDate = dateOfBirth;	
+	}
+		
+	//Ќазначить пол пользовател€
+	public void setGender(char gen) {
+		gender = gen;
+	}
+	
+	//Ќазначиьт рейтинг пользовател€
+	public void setRating(int Rating) {
+		rating = Rating;
+	}
+	
+	//ѕолучить айди пользовател€
+	public int getId() {	
+		return id;	
+	}
+	
+	//ѕолучить им€ пользовател€
+	public String getName() {	
+		return name;	
+	}
+	
+	//ѕолучить дату рождени€ пользовател€
+	public LocalDate getBirthDate() {	
+		return birthDate;	
+	}
+	
+	//ѕолучить пол пользовател€
+	public char getGender() {	
+		return gender;
+	}
+	
+	//получить рейтинг пользовател€
+	public int getRating() {
+		return rating;
+	}
+	public String getQuestion () throws SQLException {
+		DataBaseManager.Connect();
+		String res = null;
+		res = DataBaseManager.BDGetQuestion(id);
+		DataBaseManager.Disconnect();
+		return res;
+	}
+	public boolean checkAnswer(String answer) throws SQLException {
+		DataBaseManager.Connect();
+		String answerBD = null;
+		answerBD = DataBaseManager.BDGetAnswer(id);
+		
+		DataBaseManager.Disconnect();
+		if(answerBD.equals(answer)) {
+			return true;
+		} else
+			return false;
+	}
 }
