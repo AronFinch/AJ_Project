2e173c63e946f9477fc236ef0fc76681c234ce77
karpo <karpo@192.168.1.7>
2016-07-07 15:50:52 +0300

    Костыльный метод проверки полей ввода.

--- kritkod/src/application/ControllerDialogLogin.java
+++ kritkod/src/application/ControllerDialogLogin.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.xml.stream.EventFilter;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -13,6 +15,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.DataBaseManager;
@@ -25,66 +29,166 @@ public class ControllerDialogLogin {
     private TextField Login;
 	@FXML
     private TextField Password;
+	@FXML
+	private Label LoginHelp;
+	@FXML
+	private Label PasswordHelp;
 	
 	public Controller children;  // Ссылка на контроллер поражаемой формы
 	public Controller parent;     // Ссылка на родительский контроллер (если таковой есть для данной формы)
 	
+	private boolean loginOk;
+	private boolean passwordOk;
+	
+	@FXML
+	private void LoginKeyPress(KeyEvent kf) {
+		String key = kf.getCode().getName();
+		String log = Login.getText();
+		if(key.equals("Enter") && loginOk) {
+			if(log.length() > 0) {
+				Password.requestFocus();
+			} else {
+				loginOk = false;
+				LoginHelp.setText("Логин не может быть пустым!");
+				LoginHelp.setOpacity(1);
+				LoginHelp.setDisable(false);
+			}
+		} else {
+			if(log.length() <= 20) {
+				loginOk = true;
+				LoginHelp.setOpacity(0);
+				LoginHelp.setDisable(true);
+				for(int i = 0; i < log.length(); i++) {
+					if(log.charAt(i) == ' ') {
+						loginOk = false;
+						LoginHelp.setText("Логин не должен содержать пробелы!");
+						LoginHelp.setOpacity(1);
+						LoginHelp.setDisable(false);
+						break;
+					} else {
+						LoginHelp.setOpacity(0);
+						LoginHelp.setDisable(true);
+						loginOk = true;
+					}
+				}
+			} else {
+				loginOk = false;
+				LoginHelp.setText("Логин не превышает 20 символов!");
+				LoginHelp.setOpacity(1);
+				LoginHelp.setDisable(false);
+			}
+		}		
+	}
+	@FXML
+	private void PasswordKeyPress(KeyEvent kf) throws IOException {
+		String key = kf.getCode().getName();
+		String pass = Password.getText();
+		System.out.println(pass);
+		if(key.equals("Enter") && passwordOk) {
+			if(pass.length() > 0) {
+				ClickedLogin();
+			} else {
+				passwordOk = false;
+				PasswordHelp.setText("Пароль не может быть пустым!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+			}
+		} else {
+			if(pass.length() <= 20) {
+				passwordOk = true;
+				PasswordHelp.setOpacity(0);
+				PasswordHelp.setDisable(true);
+				for(int i = 0; i < pass.length(); i++) {
+					if(pass.charAt(i) == ' ') {
+						passwordOk = false;
+						PasswordHelp.setText("Пароль не должен содержать пробелы!");
+						PasswordHelp.setOpacity(1);
+						PasswordHelp.setDisable(false);
+						break;
+					} else {
+						PasswordHelp.setOpacity(0);
+						PasswordHelp.setDisable(true);
+						passwordOk = true;
+					}
+				}
+			} else {
+				loginOk = false;
+				LoginHelp.setText("пароль не превышает 20 символов!");
+				LoginHelp.setOpacity(1);
+				LoginHelp.setDisable(false);
+			}
+		}		
+	}
 	@FXML
 	private void ClickedLogin() throws IOException{
 		
 		// проверка полей на пустоту
-		if(!Login.getText().equals("")) {
+		if(!Login.getText().equals("")) {	
+			Password.requestFocus();
 			if(!Password.getText().equals("")) {
-				try {
-					if(Main.mainUser.loadUser(Login.getText(), Password.getText())) {//Загружаем пользователя
+				if(loginOk && passwordOk) {
+					LoginHelp.setOpacity(0);
+					LoginHelp.setDisable(true);
+					PasswordHelp.setOpacity(0);
+					PasswordHelp.setDisable(true);
+					try {
+						if(Main.mainUser.loadUser(Login.getText(), Password.getText())) {//Загружаем пользователя
 						//тут загрузить список пользователей
-						ArrayList<String> logins;
+							ArrayList<String> logins;
 						
-						DataBaseManager.Connect();
-						logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
-						DataBaseManager.Disconnect();
+							DataBaseManager.Connect();
+							logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
+							DataBaseManager.Disconnect();
 						
-						for(int i = 0; i < logins.size(); i++) {
-							User user = new User();
-							user.loadUser(logins.get(i));
-							Main.otherUsers.add(user);
-						}
+							for(int i = 0; i < logins.size(); i++) {
+								User user = new User();
+								user.loadUser(logins.get(i));
+								Main.otherUsers.add(user);
+							}
 						//загрузили список пользователей
-						Main.primaryStage.close();
+							Main.primaryStage.close();
 						
-						Stage stage = new Stage();
-						Main.primaryStage = stage;
-					 	Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
-						stage.setScene(new Scene(root));
-						stage.setTitle("Главный экран:");
-						stage.show();
-					} else {
-						Login.clear();
-						Password.clear();
-						Alert alert = new Alert(Alert.AlertType.INFORMATION);
+							Stage stage = new Stage();
+							Main.primaryStage = stage;
+					 		Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
+					 		stage.setScene(new Scene(root));
+							stage.setTitle("Главный экран:");
+							stage.show();
+						} else {
+							LoginHelp.setText("Такого пользователя не существует!");
+							LoginHelp.setOpacity(1);
+							LoginHelp.setDisable(false);
+						
+							PasswordHelp.setText("Или пароль введён неверно!");
+							PasswordHelp.setOpacity(1);
+							PasswordHelp.setDisable(false);
+						
+							Login.requestFocus();
+						}	
+					} catch (SQLException e) {
+						Main.mainUser.clear();
+						Alert alert = new Alert(Alert.AlertType.ERROR);
 						alert.setTitle("Information");
-					    alert.setHeaderText("Такого пользователя нет или пароль введён не верно!");
-					    alert.showAndWait();
-					}	
-				} catch (SQLException e) {
-					Main.mainUser.clear();
-					Alert alert = new Alert(Alert.AlertType.ERROR);
-					alert.setTitle("Information");
-				    alert.setHeaderText("Error!");
-				    alert.setContentText(e.getMessage());
-				    alert.showAndWait();
+				    	alert.setHeaderText("Error!");
+				    	alert.setContentText(e.getMessage());
+				    	alert.showAndWait();
+					}
 				}
 			} else {
-				Alert alert = new Alert(Alert.AlertType.INFORMATION);
-				alert.setTitle("Information");
-			    alert.setHeaderText("Поле Password не должно быть пустым!");
-			    alert.showAndWait();
+				PasswordHelp.setText("Пароль не иожет быть пустым!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+				Password.requestFocus();
 			}
 		} else {
-			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			LoginHelp.setText("Логин не может быть пустым!");
+			LoginHelp.setOpacity(1);
+			LoginHelp.setDisable(false);
+			Login.requestFocus();
+/*			Alert alert = new Alert(Alert.AlertType.INFORMATION);
 			alert.setTitle("Information");
 		    alert.setHeaderText("Поле Login не должно быть пустым!");
-		    alert.showAndWait();
+		    alert.showAndWait();*/
 		}// конец проверки с выводом сообщения
 		
 	}
