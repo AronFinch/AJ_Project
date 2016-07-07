d823a25e499a3758b40359aeda9fd5e8be3ead07
karpo <karpo@192.168.1.7>
2016-07-08 02:54:43 +0300

    Добавил ограничение на форму изменения пароля при восствновлении.

--- kritkod/src/application/ControllerDialogLoginNewPassword.java
+++ kritkod/src/application/ControllerDialogLoginNewPassword.java
@@ -10,8 +10,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import model.DataBaseManager;
 import model.User;
@@ -23,52 +25,117 @@ public class ControllerDialogLoginNewPassword {
 	private PasswordField NewPassword;
 	@FXML
 	private PasswordField NewPassword2;
-	
+	@FXML
+	private Label PasswordHelp;
+	private boolean PasswordOk;
 
 	@FXML
-    public void ButtonAction(ActionEvent actionEvent) throws IOException{
-		if(NewPassword.getText().equals(NewPassword2.getText())) {
-			try {
-				Main.mainUser.changePassword(NewPassword.getText());
-				//на этом моменте выполнить переход
-				//прогрузил других пользователей
-				ArrayList<String> logins;
-				
-				DataBaseManager.Connect();
-				logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
-				DataBaseManager.Disconnect();
-				
-				for(int i = 0; i < logins.size(); i++) {
-					User user = new User();
-					user.loadUser(logins.get(i));
-					Main.otherUsers.add(user);
-				}
-				Main.primaryStage.close();
-				
-				Stage stage = new Stage();
-				Main.primaryStage = stage;
-			 	Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
-			 	root.getStylesheets().clear();
-			 	root.getStylesheets().add(Main.style);
-				stage.setScene(new Scene(root));
-				stage.setTitle("Главный экран:");
-				stage.show();
-			} catch (SQLException e) {
-				Alert alert = new Alert(Alert.AlertType.ERROR);
-				alert.setTitle("Information");
-	    	    alert.setHeaderText("Error!");
-	    	    alert.setContentText(e.getMessage());
-	    	    alert.showAndWait();
+	public void PasswordKeyPress(KeyEvent fk) throws IOException {
+		String key = fk.getCode().getName();
+		String password;
+		
+		if(NewPassword.isFocused())
+			password = NewPassword.getText();
+		else 
+			password = NewPassword2.getText();
+		
+		
+		if(key.equals("Enter") && PasswordOk) {
+			if(password.length() > 0) {
+				if(NewPassword.isFocused())
+					NewPassword2.requestFocus();
+				else
+					ButtonAction();
+			} else {
+				PasswordHelp.setText("Пароль не должен быть пустым!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
 			}
 		} else {
-			NewPassword.clear();
-			NewPassword2.clear();
-			Alert alert = new Alert(Alert.AlertType.INFORMATION);
-			alert.setTitle("Information");
-    	    alert.setHeaderText("Неверныйе пароли!");
-    	    alert.setContentText("Попробуйте ещё раз.");
-    	    alert.showAndWait();
+			if(password.length() <= 20) {
+				PasswordOk = true;
+				PasswordHelp.setOpacity(0);
+				PasswordHelp.setDisable(true);
+				for(int i = 0; i < password.length(); i++) {
+					if(password.charAt(i) == ' ') {
+						PasswordOk = false;
+						PasswordHelp.setText("Пароль не должен содержать пробелы!");
+						PasswordHelp.setOpacity(1);
+						PasswordHelp.setDisable(false);
+						break;
+					}
+				}
+			} else {
+				PasswordOk = false;
+				PasswordHelp.setText("Пароль не должен превышать 20 символов!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+			}
 		}
 	}
-
+	
+	@FXML
+    public void ButtonAction() throws IOException{
+		String password1, password2;
+		password1 = NewPassword.getText();
+		password2 = NewPassword2.getText();
+		
+		if(password2.equals("")) {
+			PasswordOk = false;
+			PasswordHelp.setText("Пароль не должен быть пустым!");
+			PasswordHelp.setOpacity(1);
+			PasswordHelp.setDisable(false);
+			NewPassword.requestFocus();
+		}
+		if(password1.equals("")) {
+			PasswordOk = false;
+			PasswordHelp.setText("Пароль не должен быть пустым!");
+			PasswordHelp.setOpacity(1);
+			PasswordHelp.setDisable(false);
+			NewPassword2.requestFocus();
+		}
+		if(PasswordOk) {
+			//начинаем проверку двух паролей
+			if(password1.equals(password2)) {
+				try {
+					Main.mainUser.changePassword(password1);
+					ArrayList<String> logins;
+					
+					DataBaseManager.Connect();
+					logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
+					DataBaseManager.Disconnect();
+					
+					for(int i = 0; i < logins.size(); i++) {
+						User user = new User();
+						user.loadUser(logins.get(i));
+						Main.otherUsers.add(user);
+					}
+					Main.primaryStage.close();
+					
+					Stage stage = new Stage();
+					Main.primaryStage = stage;
+				 	Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
+				 	root.getStylesheets().clear();
+				 	root.getStylesheets().add(Main.style);
+					stage.setScene(new Scene(root));
+					stage.setTitle("Главный экран:");
+					stage.show();
+				} catch (SQLException e) {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setTitle("Information");
+		    	    alert.setHeaderText("Error!");
+		    	    alert.setContentText(e.getMessage());
+		    	    alert.showAndWait();
+				}
+			} else {
+				PasswordOk = false;
+				PasswordHelp.setText("Вы ошиблись при второй попытке ввода!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+				NewPassword.clear();
+				NewPassword2.clear();
+				NewPassword.requestFocus();
+			}
+		}
+	}
 }
