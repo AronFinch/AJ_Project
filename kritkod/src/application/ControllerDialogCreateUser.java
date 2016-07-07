9cdd0ba235af5867cfe58579678f8d475fdc90a3
karpo <karpo@192.168.1.7>
2016-07-07 21:49:22 +0300

    Ограничения на полях регистрации.

--- kritkod/src/application/ControllerDialogCreateUser.java
+++ kritkod/src/application/ControllerDialogCreateUser.java
@@ -12,10 +12,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import model.DataBaseManager;
 import model.User;
@@ -53,86 +55,233 @@ public class ControllerDialogCreateUser {
 	private ToggleGroup gender;
 	
 	@FXML
-    public void addNewUser(ActionEvent actionEvent) throws IOException {
+	private Label LoginHelp;
+	@FXML
+	private Label PasswordHelp;
+	@FXML
+	private Label NameHelp;
+	@FXML
+	private Label GenderHelp;
+	@FXML
+	private Label DateHelp;
+	
+	private boolean LoginOk;
+	private boolean PasswordOk;
+	private boolean NameOk;
+	private boolean GenderOk;
+	private boolean DateOk;
+	
+	@FXML
+	private void LoginKeyPress(KeyEvent kf) {
+		String key = kf.getCode().getName();
+		String login = addUserLogin.getText();
+		if(key.equals("Enter") && LoginOk) {
+			if(login.length() > 0)
+				addUserPassword.requestFocus();
+			else {
+				LoginOk = false;
+				LoginHelp.setText("Логин не должен быть пустым!");
+				LoginHelp.setOpacity(1);
+				LoginHelp.setDisable(false);
+			}
+		} else {
+			if(login.length() <= 20) {
+				LoginOk = true;
+				LoginHelp.setOpacity(0);
+				LoginHelp.setDisable(true);
+				for(int i = 0; i < login.length(); i++) {
+					if(login.charAt(i) == ' ') {
+						LoginOk = false;
+						LoginHelp.setText("Логин не должен содержать пробелы!");
+						LoginHelp.setOpacity(1);
+						LoginHelp.setDisable(false);
+						break;
+					}
+				}
+			} else {
+				LoginOk = false;
+				LoginHelp.setText("Логин не должен превышать 20 символов!");
+				LoginHelp.setOpacity(1);
+				LoginHelp.setDisable(false);
+			}
+		}
+	}
+	@FXML
+	private void PasswordKeyPress(KeyEvent kf) {
+		String key = kf.getCode().getName();
+		String password = addUserPassword.getText();
+		if(key.equals("Enter") && PasswordOk) {
+			if(password.length() > 0)
+				addNameUser.requestFocus();
+			else {
+				PasswordOk = false;
+				PasswordHelp.setText("Пароль не должен быть пустыи!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+			} 		
+		} else {
+			if(password.length() <= 20) {
+				PasswordOk = true;
+				PasswordHelp.setOpacity(0);
+				PasswordHelp.setDisable(true);
+				for(int i = 0; i < password.length(); i++) {
+					if(password.charAt(i) == ' ') {
+						PasswordOk = false;
+						PasswordHelp.setText("Пароль не должен содержать пробелы!");
+						PasswordHelp.setOpacity(1);
+						PasswordHelp.setDisable(false);
+					}
+				}
+			} else {
+				PasswordOk = false;
+				PasswordHelp.setText("Пароль не должен превышать 20 символов!");
+				PasswordHelp.setOpacity(1);
+				PasswordHelp.setDisable(false);
+			}
+		}
+	}
+	@FXML
+	private void NameKeyPress(KeyEvent kf) {
+		String key = kf.getCode().getName();
+		String name = addNameUser.getText();
+		if(key.equals("Enter") && NameOk) {
+			if(name.length() > 0)
+				addDataBirthDate.requestFocus();
+			else {
+				NameOk = false;
+				NameHelp.setText("Имя не должно быть пустым!");
+				NameHelp.setOpacity(1);
+				NameHelp.setDisable(false);
+			}
+		} else {
+			if(name.length() <= 50) {
+				NameOk = false;
+				NameHelp.setText("Имя не должно состоять из пробелов!");
+				NameHelp.setOpacity(1);
+				NameHelp.setDisable(false);
+				for(int i = 0; i < name.length(); i++) {
+					if(name.charAt(i) != ' ') {
+						NameOk = true;
+						NameHelp.setOpacity(0);
+						NameHelp.setDisable(true);
+					}
+				}
+			} else {
+				NameOk = false;
+				NameHelp.setText("Имя не должно первышать 50 символов!");
+				NameHelp.setOpacity(1);
+				NameHelp.setDisable(false);
+			}
+		}
+	}
+	@FXML
+	private void DateKeyPress(KeyEvent kf) throws IOException {
+		String key = kf.getCode().getName();
+		LocalDate date = addDataBirthDate.getValue();
+		if(key.equals("Enter")) {
+			addNewUser();
+		} 
+	}
+	@FXML
+    public void addNewUser() throws IOException {
 		String login = addUserLogin.getText();
 		String password = addUserPassword.getText();
 		String name = addNameUser.getText();
 		LocalDate birthDate = addDataBirthDate.getValue();
 		char userGender = 'h';
-		// тут будет многоуровневая конструкций условий
-		if(!login.equals("") && !password.equals("")) { 
-			// логин и пароль введены
-			if(!name.equals("")) {
-				if(gender.getSelectedToggle() != null) {
-					if(birthDate != null) {
-						if(gender.getSelectedToggle() == addGenderMale)
-							userGender = 'm';
-						else
-							userGender = 'f';
-						Main.mainUser.setBirthDate(birthDate);
-						Main.mainUser.setName(name);
-						Main.mainUser.setGender(userGender);
-						try {
-							if(Main.mainUser.SaveUser(login, password, SQ.getText(), answer.getText())) {
-								// переход на главный экран
-								// прогрузить остальных пользователей
-								ArrayList<String> logins;
-								
-								DataBaseManager.Connect();
-								logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
-								DataBaseManager.Disconnect();
-								
-								for(int i = 0; i < logins.size(); i++) {
-									User user = new User();
-									user.loadUser(logins.get(i));
-									Main.otherUsers.add(user);
-								}
-								Main.primaryStage.close();
-								
-								Stage stage = new Stage();
-								Main.primaryStage = stage;
-							 	Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
-								stage.setScene(new Scene(root));
-								stage.setTitle("Главный экран:");
-								stage.show();
-							} else {
-								Main.mainUser.clear();
-								Alert alert = new Alert(Alert.AlertType.INFORMATION);
-								alert.setTitle("Information");
-					    	    alert.setHeaderText("Пользователь с таким логином уже существует!");
-					    	    alert.showAndWait();
-							}
-						} catch (SQLException e) {
-							Main.mainUser.clear();
-							Alert alert = new Alert(Alert.AlertType.ERROR);
-							alert.setTitle("Information");
-				    	    alert.setHeaderText("Error!");
-				    	    alert.setContentText(e.getMessage());
-				    	    alert.showAndWait();
-						}
-					} else {
-						Alert alert = new Alert(Alert.AlertType.INFORMATION);
-						alert.setTitle("Information");
-			    	    alert.setHeaderText("Вы не ввели дату рождения!");
-			    	    alert.showAndWait();
-					}
-				} else {
-					Alert alert = new Alert(Alert.AlertType.INFORMATION);
-					alert.setTitle("Information");
-		    	    alert.setHeaderText("Вы не выбрали пол!");
-		    	    alert.showAndWait();
-				}
+		
+		if(gender.getSelectedToggle() != null) {
+			GenderOk = true;
+			if(addGenderMale.equals(true))
+				userGender = 'm';
+			else
+				userGender = 'f';
+		} else {
+			GenderOk = false;
+			GenderHelp.setText("Выберите пол!");
+			GenderHelp.setOpacity(1);
+			GenderHelp.setDisable(false);
+		}
+		if(birthDate != null) {
+			if(birthDate.isBefore(LocalDate.now())) {
+				DateOk = true;
 			} else {
-				Alert alert = new Alert(Alert.AlertType.INFORMATION);
-				alert.setTitle("Information");
-	    	    alert.setHeaderText("Пустое поле имени!");
-	    	    alert.showAndWait();
+				DateOk = false;
+				DateHelp.setText("Вы не могли родиться в будущем!");
+				DateHelp.setOpacity(1);
+				DateHelp.setDisable(false);
 			}
 		} else {
-			Alert alert = new Alert(Alert.AlertType.INFORMATION);
-			alert.setTitle("Information");
-    	    alert.setHeaderText("Пустое поле логина или пароля!");
-    	    alert.showAndWait();
+			DateOk = false;
+			DateHelp.setText("Введите дату рождения!");
+			DateHelp.setOpacity(1);
+			DateHelp.setDisable(false);
+			addDataBirthDate.requestFocus();
+		}
+		if(!name.equals("")) {
+			NameOk = true;
+		} else {
+			NameOk = false;
+			NameHelp.setText("Имя не должно быть пустым!");
+			NameHelp.setOpacity(1);
+			NameHelp.setDisable(false);
+			addNameUser.requestFocus();
+		}
+		if(!password.equals("")) {
+			PasswordOk = true;
+		} else {
+			PasswordOk = false;
+			PasswordHelp.setText("Пароль не должен быть пустым!");
+			PasswordHelp.setOpacity(1);
+			PasswordHelp.setDisable(false);
+			addUserPassword.requestFocus();
+		}
+		if(!login.equals("")) {
+			LoginOk = true;
+		} else {
+			LoginOk = false;
+			LoginHelp.setText("Логин не должен быть пустым!");
+			LoginHelp.setOpacity(1);
+			LoginHelp.setDisable(false);
+			addUserLogin.requestFocus();
+		}
+		if(LoginOk && PasswordOk && NameOk && GenderOk && DateOk) {
+			Main.mainUser.setName(name);
+			Main.mainUser.setGender(userGender);
+			Main.mainUser.setBirthDate(birthDate);
+			Main.mainUser.setRating(100);
+			try {
+				if(Main.mainUser.SaveUser(login, password, SQ.getText(), answer.getText())) {
+					// переход на главный экран
+					// прогрузить остальных пользователей
+					ArrayList<String> logins;
+					
+					DataBaseManager.Connect();
+					logins = DataBaseManager.BDGetOtherLogins(Main.mainUser.getId());
+					DataBaseManager.Disconnect();
+					
+					for(int i = 0; i < logins.size(); i++) {
+						User user = new User();
+						user.loadUser(logins.get(i));
+						Main.otherUsers.add(user);
+					}
+					Main.primaryStage.close();
+					
+					Stage stage = new Stage();
+					Main.primaryStage = stage;
+				 	Parent root = FXMLLoader.load(Controller.class.getResource("FXMLDocument.fxml"));
+					stage.setScene(new Scene(root));
+					stage.setTitle("Главный экран:");
+					stage.show();
+				}
+			} catch (SQLException e) {
+				Main.mainUser.clear();
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setTitle("Information");
+	    	    alert.setHeaderText("Error!");
+	    	    alert.setContentText(e.getMessage());
+	    	    alert.showAndWait();
+			}
 		}
 	}
 	
